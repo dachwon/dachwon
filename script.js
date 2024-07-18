3829dbfcaa74da36cc557dae622c862648542c6c
dachwon <k.yokerkhoff@gmail.com>
2024-07-18 10:12:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const favicon = document.getElementById('favicon');
     favicon.classList.add('rotating');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    function typeWriter(element, text, delay) {
+        let i = 0;
+        function type() {
+            if (i < text.length) {
+                element.innerHTML += text.charAt(i);
+                i++;
+                setTimeout(type, delay);
+            }
+        }
+        type();
+    }
+
+    const typewriterElements = document.querySelectorAll(".typewriter");
+
+    typewriterElements.forEach(element => {
+        const text = element.textContent;
+        element.textContent = "";
+        typeWriter(element, text, 100);
+    });
+});
