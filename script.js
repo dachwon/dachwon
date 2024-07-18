be4825b92b8fc051fb4182a38fb867bbbc72cc16
dachwon <k.yokerkhoff@gmail.com>
2024-07-18 06:14:44 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const playButton = document.getElementById('play-music');
+    const iframe = document.getElementById('youtube-music');
+
+    playButton.addEventListener('click', () => {
+        iframe.style.display = 'block';
+        playButton.style.display = 'none';
+    });    
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const favicon = document.getElementById('favicon');
+    favicon.classList.add('rotating');
+});
